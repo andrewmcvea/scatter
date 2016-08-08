7a2ebe75d52e7b54d95c3b54fc0a50619bfc192b
andrewmcvea <andrewmcvea@uchicago.edu>
2016-08-08 11:43:53 -0500

    Complete rewrite
    Changed the method for calculating time resolution and amplitudes. Most borrowed from timing.py. Also is now able to plot a scatter plot since the time resolution and amplitudes are the same sized arrays.

--- scatter.py
+++ scatter.py
@@ -3,64 +3,80 @@ from __future__ import division
 import h5py
 import numpy as np
 from zmq_client import adc_to_voltage
-import sys
-from scipy.optimize import fmin
 
 print "start program"
 
-#Finds the amplitude of each of the curves with voltage barrier
-def find_amp(v):
-        amplitude = np.min(v,axis=1)
-        filteramp = amplitude[amplitude < -200]
-        return abs(amplitude)
+def get_times(y, fraction=0.4):
+    """
+    Returns pulse times in `y` by looking for the pulse
+    to cross a constant fraction `fraction` of the pulse height in each
+    waveform. `y` should be a 2 dimensional array with shape (N,M) where
+    N is the number of waveforms, and M is the number of samples per
+    waveform.
+    """
+    # samples below threshold
+    mask1 = y > np.min(y,axis=-1)[:,np.newaxis]*fraction
+    # samples before the minimum
+    mask2 = np.arange(y.shape[1]) < np.argmin(y,axis=-1)[:,np.newaxis]
 
-#Finds the time resolution of the associated voltages
-def find_res(v):
-    t = np.empty(v.shape[0],dtype=float)
-    for i in range(len(v)):
-        if i % 100 == 0:
-            print "\r%i/%i" % (i+1,len(v)),
-            sys.stdout.flush()
-        j = np.argmin(v[i])
-        disc = v[i,j]*0.4
-        while v[i,j] < disc and j > 0:
-            j -= 1
-        t[i] = j + (disc - v[i,j])/(v[i,j+1] - v[i,j])
-    print()
-    return t
+    # right side of threshold crossing
+    r = y.shape[1] - np.argmax((mask1 & mask2)[:,::-1], axis=-1)
+    r[r == 0] = 1
+    r[r == y.shape[1]] = y.shape[1] - 1
+    l = r - 1
 
-#Takes the input from the terminal and reads it
+    yl = y[np.arange(y.shape[0]),l]
+    yr = y[np.arange(y.shape[0]),r]
+
+    return (np.min(y,axis=-1)*fraction - yl)/(yr-yl) + l
+
+def fft_filter(y, dt, cutoff=500e6):
+    """
+    Filter the array `y` by removing frequency components above
+    `cutoff` in Hz.
+    """
+    out = np.fft.rfft(y)
+    freq = np.fft.rfftfreq(y.shape[1], d=dt)
+    out[:,freq > cutoff] = 0
+    return np.fft.irfft(out)
+
 if __name__ == '__main__':
     import argparse
+    import sys
     import matplotlib.pyplot as plt
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename', help="input filename")
+    parser.add_argument('-o', dest='output', default='out.txt')
+    parser.add_argument('filenames', nargs='+', help='input files')
+    parser.add_argument('-c', '--chunk', type=int, default=10000)
     args = parser.parse_args()
 
-    f = h5py.File(args.filename)
-    dset = f['c1'][:100000]
-    amp = find_amp(dset)
-    f_dset = dset[amp > 200]
+#Calculate the time resolution for each pulse and creates an array of these values
+    t = []
+    amp = []
+    amp2 = []
+    for filename in args.filenames:
+        with h5py.File(filename) as f:
+            for i in range(0, f['c1'].shape[0], args.chunk):
+                y1 = adc_to_voltage(f['c1'][i:i+args.chunk])
+                y2 = adc_to_voltage(f['c2'][i:i+args.chunk])
+                # only accept c2 events below -10 mV
+                mask = np.min(y2,axis=-1) < -10e-3
+                #tony's new line
+                mask &= np.min(y1, axis=-1) < -100e-3
+                y1 = y1[mask]
+                y2 = y2[mask]
+                t1 = get_times(y1)*0.5 # sample -> ns
+                t2 = get_times(y2)*0.5
+                res = t2 - t1
+                t.extend(res)
+                amp.extend(y1)
+                amp2.extend(y2)
 
-    t1 = find_res(f_dset)
-    t = t1.copy()
-    t *= 0.5
-    t -= np.mean(t)
-
-    dset2 = f['c2'][:100000]
-    amp2 = find_amp(dset2)
-    f_dset2 = dset[amp > 200]
-
-    t0 = find_res(f_dset2)
-    t2 = t0.copy()
-    t2 *= 0.5
-    t2 -= np.mean(t)
-
-    plt.scatter(f_dset, t, color='blue')
-    plt.scatter(f_dset2, t2, color='red')
-    plt.xlabel("Amplitude")
+    plt.scatter(amp, t, color='blue')
+    plt.scatter(amp2, t, color='red')
+    plt.xlabel("Amplitudes")
     plt.ylabel("Time Resolution")
     plt.title("Time Resolution vs. Amplitude")
 
-plt.show() 
+plt.show()
